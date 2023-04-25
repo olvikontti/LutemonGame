b79bdb9165b4ef27373a1d3e392ee25db4db995f
akaKidV <villesrinne@gmail.com>
2023-04-25 18:34:47 +0300

    Main activty ja Add activity näkymät

--- app/src/main/java/com/example/lutemongame/MainActivity.java
+++ app/src/main/java/com/example/lutemongame/MainActivity.java
@@ -2,17 +2,44 @@ package com.example.lutemongame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = MainActivity.this;
+
     }
 
-    private String vittu;
+    public void switchToAddLutemon(View view) {
+        Intent intent = new Intent(this, AddLutemonActivity.class);
+        startActivity(intent);
+    }
+
+    public void switchToLutemonList(View view) {
+        Intent intent = new Intent(this, LutemonListActivity.class);
+        startActivity(intent);
+    }
+
+    public void switchToMoveLutemons(View view) {
+        Intent intent = new Intent(this, MoveLutemonsActivity.class);
+        startActivity(intent);
+    }
+
+    public void switchToBattle(View view) {
+        Intent intent = new Intent(this, BattleActivity.class);
+        startActivity(intent);
+    }
+
 }
 
 //testi

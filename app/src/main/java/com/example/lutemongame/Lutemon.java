e80a5d014dd049b22ead815ace770f9e796f6a2c
rlatt <rasmus.lattu@gmail.com>
2023-04-25 19:08:51 +0300

    lutemon.java

--- app/src/main/java/com/example/lutemongame/Lutemon.java
+++ app/src/main/java/com/example/lutemongame/Lutemon.java
@@ -1,7 +1,21 @@
 package com.example.lutemongame;
 
 public class Lutemon {
-}
+    protected String name;
+    protected String color;
+    protected int age;
+    protected int gameNumber;
+    protected int attack;
+    protected int defense;
+    protected int health;
+    protected int maxHealth;
+    protected int id;
 
-//koodia jeeeeeee
-//testi uudestaan
+    public Lutemon(String name, String color, int age, int gameNumber) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.gameNumber = gameNumber;
+    }
+
+}

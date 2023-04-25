3b021fcefa3403bb8a068dbe4f5903dd8d8030c7
rlatt <rasmus.lattu@gmail.com>
2023-04-25 18:32:17 +0300

    storage

--- app/src/main/java/com/example/lutemongame/LutemonStorage.java
+++ app/src/main/java/com/example/lutemongame/LutemonStorage.java
@@ -1,4 +1,32 @@
 package com.example.lutemongame;
 
+import java.util.ArrayList;
+
 public class LutemonStorage {
+    private String name;
+    private ArrayList<Lutemon> lutemons = new ArrayList<>();
+    private static LutemonStorage storage = null;
+
+    private LutemonStorage() {
+    }
+
+    public void addLutemon(Lutemon lutemon) {
+        lutemons.add(lutemon);
+    }
+
+    public Lutemon getLutemon(int id) {
+        return lutemons.get(id);
+    }
+
+    public ArrayList<Lutemon> getLutemons() {
+        return lutemons;
+    }
+
+    public static LutemonStorage getInstance() {
+        if (storage == null) {
+            storage = new LutemonStorage();
+        }
+        return storage;
+    }
+
 }

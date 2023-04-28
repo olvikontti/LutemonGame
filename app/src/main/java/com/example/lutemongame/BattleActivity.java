7c7e6a34f8bd4ae7b31ba1645aad6dafcab392b1
rlatt <rasmus.lattu@gmail.com>
2023-04-28 17:14:47 +0300

    battleactivity

--- app/src/main/java/com/example/lutemongame/BattleActivity.java
+++ app/src/main/java/com/example/lutemongame/BattleActivity.java
@@ -7,8 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class BattleActivity extends AppCompatActivity {
+
+    private ArrayList<String> lutemonsForBattle = new ArrayList<>();
+    private TextView battleTxt;
+    private int lutemonId1;
+    private int LutemonId2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,7 @@ public class BattleActivity extends AppCompatActivity {
 
         LinearLayout listLayout = findViewById(R.id.trainingListLayout);
         LinearLayout battleLayout = findViewById(R.id.battleLayout);
+        battleTxt = findViewById(R.id.txtBattle);
 
         // Creates the checkbox list of available lutemons
         for (int i = 0; i < LutemonStorage.getInstance().getSize(); i++) {
@@ -32,6 +41,20 @@ public class BattleActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (battleLayout.getChildCount() > 0) {
                     battleLayout.removeAllViews();
+                }
+                lutemonsForBattle.clear();
+
+                // Adds lutemons to list
+                for (int i = 0; i < listLayout.getChildCount(); i++) {
+                    View v = listLayout.getChildAt(i);
+                    if (view instanceof CheckBox) {
+                        CheckBox cb = (CheckBox) view;
+                        if (cb.isChecked()) {
+                            int id = cb.getId();
+                            String name = LutemonStorage.getInstance().getLutemon(id).getName();
+                            lutemonsForBattle.add(name);
+                        }
+                    }
                 }
             }
         });

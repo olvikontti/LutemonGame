a7352f6e305eca89ba78aa1fdca62eb67531fc8a
rlatt <rasmus.lattu@gmail.com>
2023-04-28 14:35:55 +0300

    radioshit vol20

--- app/src/main/java/com/example/lutemongame/TrainLutemonActivity.java
+++ app/src/main/java/com/example/lutemongame/TrainLutemonActivity.java
@@ -3,6 +3,10 @@ package com.example.lutemongame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class TrainLutemonActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class TrainLutemonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train_lutemon);
+
+        RadioGroup rgLutemonlist = findViewById(R.id.rgTrainingList);
+
+        for (int i = 0; i < LutemonStorage.getInstance().getSize(); i++) {
+            RadioButton rb = new RadioButton(this);
+            rb.setId(i);
+            rb.setText(LutemonStorage.getInstance().getLutemon(i).getName());
+            rgLutemonlist.addView(rb);
+        }
     }
 }

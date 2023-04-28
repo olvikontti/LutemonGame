949f8a6c1c36f3bbd2ac3dcb489aad90bda37e27
rlatt <rasmus.lattu@gmail.com>
2023-04-28 13:40:51 +0300

    scrollshit

---
+++ app/src/main/java/com/example/lutemongame/MoveFromBattleActivity.java
@@ -0,0 +1,25 @@
+package com.example.lutemongame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+public class MoveFromBattleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_from_battle);
+
+        LinearLayout listLayout = findViewById(R.id.battleListLayout);
+
+        for (int i = 0; i < LutemonStorage.getInstance().getSize(); i++) {
+            CheckBox cb = new CheckBox(this);
+            cb.setId(i);
+            cb.setText(LutemonStorage.getInstance().getLutemon(i).getName());
+            listLayout.addView(cb);
+        }
+    }
+}
